8288c9679fae8b2190174d9d4319fef672bd23d7
alvarocuadro <39199648+alvarocuadro@users.noreply.github.com>
2020-09-01 13:41:06 -0300

    Update Ldap.java

--- Ldap.java
+++ Ldap.java
@@ -5,12 +5,12 @@ import javax.naming.Context;
 
 public class Ldap {	
 		
-	Properties env = new Properties();//mantiene los par·metros de conexiÛn
+	Properties env = new Properties();//mantiene los par√°metros de conexi√≥n
 	
 	public void conectarLDAP() {	
 	env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-	env.put(Context.PROVIDER_URL, "ldap://DCSF001.finochietto.com:389");
-	env.put(Context.SECURITY_PRINCIPAL, "CN=Alvaro Cuadro,OU=XHis,OU=980-Ayacucho,OU=Accounts,DC=finochietto,DC=com");
-	env.put(Context.SECURITY_CREDENTIALS, "Paraboloide3141");}	
+	env.put(Context.PROVIDER_URL, "ldap://DomainController.com:389");
+	env.put(Context.SECURITY_PRINCIPAL, "CN del usuario");
+	env.put(Context.SECURITY_CREDENTIALS, "Password");}	
 }
 	
