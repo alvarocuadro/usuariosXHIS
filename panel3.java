963116c554ff2bf3d2370b7c165c27651e4e9df2
alvarocuadro <39199648+alvarocuadro@users.noreply.github.com>
2020-09-17 14:29:53 -0300

    Update panel3.java

--- panel3.java
+++ panel3.java
@@ -1,14 +1,10 @@
 import javax.swing.JPanel;
 import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
-
-import org.apache.commons.lang3.text.WordUtils;
-
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import java.awt.event.KeyAdapter;
@@ -20,14 +16,16 @@ import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
 
+@SuppressWarnings("serial")
 public class panel3 extends JPanel {
 	private JTextField textMatricula;
 	ClasesPanel3 clasespanel3 = new ClasesPanel3();
 	private JTextField textField;
 	private JTable tServicios;
 	String serviciosFinales = "";
-	public String nombres, apellidos, usuario, email, categoria, perfiles;	
+	public String nombres, apellidos, usuario, email, categoria, perfiles, matricula, password, tipoEmpleado, legajo, ccee, qx, prescribe, xfarma;	
 	panel2 p2 = new panel2();
 
 	/**
@@ -71,6 +69,14 @@ public class panel3 extends JPanel {
 		lblServ.setBounds(10, 67, 94, 14);
 		add(lblServ);
 		
+		JCheckBox chkCCEE = new JCheckBox("Hace CCEE");
+		chkCCEE.setBounds(161, 87, 94, 23);
+		add(chkCCEE);
+		
+		JCheckBox chkQx = new JCheckBox("Agenda Qx");
+		chkQx.setBounds(278, 87, 94, 23);
+		add(chkQx);
+		
 		textField = new JTextField();
 		textField.addKeyListener(new KeyAdapter() {
 			@Override
@@ -81,15 +87,15 @@ public class panel3 extends JPanel {
 
 				
 				DefaultTableModel dtm = new DefaultTableModel();
-				dtm.addColumn("Código");
+				dtm.addColumn("CÃ³digo");
 				dtm.addColumn("Servicio");
-				dtm.addColumn("Área");
+				dtm.addColumn("Ãrea");
 				String servicio = textField.getText();
 				int letras = servicio.length();
-		        if(letras >=3) { //realiza la acción solo al tener al menos 3 caracteres escritos
+		        if(letras >=3) { 
 				
 				Separar separa = new Separar();
-				String queryServicio = separa.separar(servicio); //envía el string escrito por el usuario a al método separar de la clase Separar para traer la query de SQL necesaria.	
+				String queryServicio = separa.separar(servicio); 
 				try {
 					
 					PreparedStatement pst=cn.prepareStatement(queryServicio);
@@ -132,7 +138,7 @@ public class panel3 extends JPanel {
 		
 		JButton btnGenUsuario = new JButton("Generar usuario");
 		btnGenUsuario.addActionListener(new ActionListener() {
-			@SuppressWarnings("deprecation")
+
 			public void actionPerformed(ActionEvent arg0) {
 				
 
@@ -160,15 +166,23 @@ else {
 	                }
 serviciosFinales=serviciosFinales.substring(0 ,serviciosFinales.length() - 1);
 }
-
+if (chkCCEE.isSelected()==true) {ccee="1";}else {ccee="";}
+if (chkQx.isSelected()==true) {qx="1";}else {qx="";}
+legajo = panel2.legajo;
+tipoEmpleado = panel2.tipoEmpleado;
+password = panel2.password;
 nombres = panel2.nombres;
 apellidos = panel2.apellidos ;
 email = panel2.email;
 usuario = panel2.usuario;
 categoria = clasespanel3.buscarPkCategoria(comboCategoria.getSelectedItem().toString());
 perfiles = clasespanel3.buscarPkPerfil(comboPerfiles.getSelectedItem().toString());
-String script = "\n Nombres: "+nombres +"\n Apellidos: "+ apellidos +"\n Email: "+email +"\n Usuario: "+ usuario+"\n Categoría: "+categoria+"\n Perfil: "+perfiles+"\n Servicios: "+serviciosFinales;
-JOptionPane.showMessageDialog(null, "Falta el script de creación de usuario en LDAP con las variables: "+script);
+matricula = textMatricula.getText();
+if(categoria.equals("1")||categoria.equals("11")||categoria.equals("12")||categoria.equals("15")||categoria.equals("16")){prescribe="1";}else {prescribe="";}
+if(perfiles.equals("21")||perfiles.equals("24")||perfiles.equals("25")||perfiles.equals("79")||perfiles.equals("132")||perfiles.equals("64")||perfiles.equals("67")||perfiles.equals("41")||perfiles.equals("63")) {xfarma = perfiles;}else {xfarma="";}
+String script = "\n Nombres: "+nombres +"\n Apellidos: "+ apellidos +"\n Email: "+email +"\n Usuario: "+ usuario+"\n Password: "+password+"\n EmployeeType: "+tipoEmpleado+"\n Legajo: "+legajo+"\n extensionAttribute3: "+ matricula+
+"\n extensionAttribute5: "+categoria+"\n extensionAttribute7: "+serviciosFinales+"\n extensionAttribute8: "+perfiles+"\n extensionAttribute9: "+ccee+"\n extensionAttribute10: "+qx+"\n extensionAttribute11: "+prescribe+"\n extensionAttribute12: "+xfarma;
+JOptionPane.showMessageDialog(null, "Falta el script de creaciÃ³n de usuario en LDAP con las variables: "+script);
 		
 				}else {
 	            	JOptionPane.showMessageDialog(null, "Debe seleccionar al menos un servicio");
@@ -181,6 +195,22 @@ JOptionPane.showMessageDialog(null, "Falta el script de creación de usuario en L
 		btnGenUsuario.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		btnGenUsuario.setBounds(396, 366, 131, 37);
 		add(btnGenUsuario);
+		
 
 		}
 }
+
+/*  
+"EmployeeNumber" = [LEGAJO]
+"EmployeeType" = [INTEXT]   
+"employeeID" = [DNI]    
+"mail" = [MAIL]
+"extensionAttribute3" = [MATNAC]    
+"extensionAttribute5" = [CATEGORIA]      
+"extensionAttribute7" = [SERVICIOS]    
+"extensionAttribute8" = [ROL]    
+"extensionAttribute9" = [AGENDA_CEX] [varchar](10) NULL,		
+"extensionAttribute10" = [AGENDA_QUIROF] [varchar] (10) NULL,    
+"extensionAttribute11" = [PRESCRIPTOR] [varchar](10) NULL,
+"extensionAttribute12" = xFARMA
+*/
